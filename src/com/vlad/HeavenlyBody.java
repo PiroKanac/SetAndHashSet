ea0262bfdcdc544e3ff9a095ef640bb7a7345a02
PiroKanac <runyoshimi@live.com>
2019-03-04 16:01:49 +0100

    First part of a challenge with creating enum

--- src/com/vlad/HeavenlyBody.java
+++ src/com/vlad/HeavenlyBody.java
@@ -8,11 +8,28 @@ public final class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
-
-    public HeavenlyBody(String name, double orbitalPeriod) {
+    //private final int bodyType;                //also need to change this in here for enum
+    private final BodyTypes bodyType;
+    //Instead of having different declarations for each define an enum
+  /*  public static final int STAR = 1;
+    public static final int PLANET = 2;
+    public static final int DWARF_PLANET = 3;
+    public static final int MOON = 4;
+    public static final int COMET = 5;
+    public static final int ASTEROID = 6; */
+    public enum BodyTypes{
+         PLANET,
+         DWARF_PLANET,
+         MOON,
+         COMET,
+         ASTEROID
+    }
+    //also for enum add here in constructor BodyTypes
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
         this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
+        this.bodyType = bodyType;
     }
 
     public String getName() {
@@ -22,8 +39,12 @@ public final class HeavenlyBody {
     public double getOrbitalPeriod() {
         return orbitalPeriod;
     }
-
-    public boolean addMoon(HeavenlyBody moon){
+    //add for challenge
+    public BodyTypes getBodyType() {
+        return bodyType;
+    }
+    //refactor for challenge
+    public boolean addSatellite(HeavenlyBody moon){
         return this.satellites.add(moon);
     }
 
@@ -32,25 +53,39 @@ public final class HeavenlyBody {
     }
 
     @Override
-    public boolean equals(Object obj){
+    public final boolean equals(Object obj){                    //add final here for challenge
         if(this == obj){
             return true;
         }
-
-        System.out.println("obj.getClass() is " + obj.getClass());
+        //don't need this for challenge
+        /*System.out.println("obj.getClass() is " + obj.getClass());
         System.out.println("this.getClass() is " + this.getClass());
         if((obj == null) || (obj.getClass() != this.getClass())){
             return false;
-        }
+        }*/
 
-        String objName = ((HeavenlyBody) obj).getName();
-        return this.name.equals(objName);
+        if(obj instanceof HeavenlyBody){
+            HeavenlyBody theObject = (HeavenlyBody) obj;
+            if(this.name.equals(theObject.getName())){
+                return this.bodyType == theObject.getBodyType();
+            }
+        }
+        return false;
+
+        /*String objName = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objName);*/
 
     }
 
     @Override
-    public int hashCode() {
-        System.out.println("hashcode called");
-        return this.name.hashCode() + 57;
+    public final int hashCode() {                         //here also add final for challenge and change code
+       /* System.out.println("hashcode called");
+        return this.name.hashCode() + 57; */
+       return this.name.hashCode() + 57 + this.bodyType.hashCode();
+    }
+    //add this for challenge
+    @Override
+    public String toString() {
+        return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
     }
 }
