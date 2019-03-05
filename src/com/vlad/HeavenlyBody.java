1ad48dc0ff1c161001cc7f68b2369659d8c4bcff
PiroKanac <runyoshimi@live.com>
2019-03-05 13:03:01 +0100

    Testcase 3,4,5,6 done for challenge

--- src/com/vlad/HeavenlyBody.java
+++ src/com/vlad/HeavenlyBody.java
@@ -5,11 +5,12 @@ import java.util.Set;
 
 public abstract class HeavenlyBody {
 
-    private final String name;
+    //private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
     //private final int bodyType;                //also need to change this in here for enum
-    private final BodyTypes bodyType;
+   // private final BodyTypes bodyType;
+    private final Key key;
     //Instead of having different declarations for each define an enum
   /*  public static final int STAR = 1;
     public static final int PLANET = 2;
@@ -26,23 +27,29 @@ public abstract class HeavenlyBody {
     }
     //also for enum add here in constructor BodyTypes
     public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
-        this.name = name;
+        this.key = new Key(name,bodyType);
+        //this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
-        this.bodyType = bodyType;
+        //this.bodyType = bodyType;
     }
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//   }
 
     public double getOrbitalPeriod() {
         return orbitalPeriod;
     }
-    //add for challenge
-    public BodyTypes getBodyType() {
-        return bodyType;
+
+    public Key getKey() {
+        return key;
     }
+
+    //add for challenge
+//    public BodyTypes getBodyType() {
+//        return bodyType;
+//    }
     //refactor for challenge
     public boolean addSatellite(HeavenlyBody moon){
         return this.satellites.add(moon);
@@ -66,9 +73,10 @@ public abstract class HeavenlyBody {
 
         if(obj instanceof HeavenlyBody){
             HeavenlyBody theObject = (HeavenlyBody) obj;
-            if(this.name.equals(theObject.getName())){
+        /*    if(this.name.equals(theObject.getName())){
                 return this.bodyType == theObject.getBodyType();
-            }
+            } */
+            return this.key.equals(theObject.getKey());
         }
         return false;
 
@@ -81,11 +89,54 @@ public abstract class HeavenlyBody {
     public final int hashCode() {                         //here also add final for challenge and change code
        /* System.out.println("hashcode called");
         return this.name.hashCode() + 57; */
-       return this.name.hashCode() + 57 + this.bodyType.hashCode();
+     //  return this.name.hashCode() + 57 + this.bodyType.hashCode();  // change again for challenge
+        return this.key.hashCode();
     }
+
+    public static Key makeKey(String name, BodyTypes bodyType){
+        return new Key(name, bodyType);
+    }
+
     //add this for challenge
     @Override
     public String toString() {
-        return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
+        return this.key.name + ": " + this.key.bodyType + ", " + this.orbitalPeriod;
+    }
+
+    public static final class Key{
+        private String name;
+        private BodyTypes bodyType;
+
+        private Key(String name, BodyTypes bodyType) {
+            this.name = name;
+            this.bodyType = bodyType;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyType() {
+            return bodyType;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+            if(this.name.equals(key.getName())){
+                return(this.bodyType == key.getBodyType());
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ": " + this.bodyType;
+        }
     }
 }
